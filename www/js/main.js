2879ba59f819a2506a012ae8cfcc4b92b1f162e1
Jeff Wu <makerjeffwu@gmail.com>
2015-11-15 05:11:07 -0800

    tried angular.bootstrap instead of ng-app directive, not working at the moment

--- www/js/main.js
+++ www/js/main.js
@@ -3,6 +3,8 @@
 //DEFINE THE APP
 var partsBinApp = angular.module('PartsBinAppName', ['ngRoute']);
 
+
+
 //configure app
 partsBinApp.config(['$routeProvider', function($routeProvider){
 
@@ -24,9 +26,21 @@ partsBinApp.config(['$routeProvider', function($routeProvider){
 partsBinApp.controller('PartsController', ['$scope','$http', '$sce', function($scope, $http, $sce) {
     $scope.name = 'Jeff';
     $scope.stuff = {value:'dummy manual input'};
+
+    //grab data via angular XHR
     $http.get('js/data.json').success(function(data) {
         $scope.bins = data; //data is a special data object, needs to be put into array
     });
-}]);
 
+    //grab the button
+    $scope.add_button = document.getElementById('add_button');
+
+    //NG-CLICK FUNCTIONS
+    // test function
+    $scope.funky = function(message) {
+        console.log(message);
+        alert(message);
+    };
+
+}]);
 
