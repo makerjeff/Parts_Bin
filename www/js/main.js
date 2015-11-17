78430f2dd5cd3a5d4d4a7ea684d12d0094a583cd
Jeff Wu <makerjeffwu@gmail.com>
2015-11-16 20:54:15 -0800

    WORKING LOCAL STORAGE SYSTEM, need to implement RnD findings into main controller

--- www/js/main.js
+++ www/js/main.js
@@ -14,53 +14,15 @@ partsBinApp.config(['$routeProvider', function($routeProvider){
     controller:'PartsController'
   });
 
+  $routeProvider.when('/local', {
+    templateUrl:'partials/local_storage.html',
+    controller:'LocalStorageController'
+  });
+
   //any other location, redirect to about page
   $routeProvider.otherwise({
     redirectTo:'/parts'
   });
 }]);
 
-
-//DEFINE CONTROLLERS
-
-partsBinApp.controller('PartsController', ['$scope','$http', '$sce', function($scope, $http, $sce) {
-   //===== CONSTRUCTORS =====
-
-    //===== VARS ======
-    $scope.name = 'Jeff';
-    $scope.stuff = {value:'dummy manual input'};
-
-    //grab data via angular XHR
-    $http.get('js/data.json').success(function(data) {
-        $scope.bins = data; //data is a special data object, needs to be put into array
-    });
-
-    //grab the button
-    $scope.add_button = document.getElementById('add_button');
-
-    //grab CREATOR div
-    $scope.creator = document.getElementById('creator');
-    $scope.creator_type = document.getElementById('input_type');
-    $scope.creator_size = document.getElementById('input_size');
-    $scope.creator_units = document.getElementById('input_units');
-    $scope.creator.style.visibility = 'hidden';
-
-    //NG-CLICK FUNCTIONS
-    // test function
-    $scope.funky = function(message) {
-        console.log(message);
-        $scope.creator.style.visibility = 'visible';
-    };
-
-    $scope.say_something = function(message) {
-       modal_popper($scope);
-    };
-
-
-
-}]);
-
-function modal_popper($scope) {
-    console.log('hello');
-}
 
